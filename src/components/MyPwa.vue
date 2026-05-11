4f2cc99a373f096ecc6e648416e5745ba6925458
ZZW <i@rayann.cn>
2026-05-11 23:40:49 +0100

    0.11.0

--- src/components/MyPwa.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import { useRegisterSW } from 'virtual:pwa-register/vue'
-
-useRegisterSW({
-  immediate: true
-})
-</script>
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import './main.css'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
